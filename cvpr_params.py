3bc35799fdb56de229a91d5834e864e83e54f677
James Bergstra <james.bergstra@gmail.com>
2011-11-08 23:37:31 -0500

    cvpr_params: different seeds for different filters

--- cvpr_params.py
+++ cvpr_params.py
@@ -68,22 +68,25 @@ lpool = {'kwargs': {'stride' : 2,
         
            
 filter1 = {'initialize': {'filter_shape' : choice([(3,3),(5,5),(7,7),(9,9)]),
-					   'n_filters' : choice([16,32,64]),
-					   'generate': ('random:uniform',{'rseed':42})},
-		   'kwargs': {'min_out' : choice([null,0]),
-					  'max_out' : choice([1,null])}}
+                          'n_filters' : choice([16,32,64]),
+                          'generate': ('random:uniform',
+                              {'rseed': choice([11, 12, 13, 14, 15])})},
+            'kwargs': {'min_out' : choice([null,0]),
+                       'max_out' : choice([1,null])}}
            
 filter2 = {'initialize': {'filter_shape' : choice([(3,3),(5,5),(7,7),(9,9)]),
-					   'n_filters' : choice([16,32,64,128]),
-					   'generate': ('random:uniform',{'rseed':42})},
-		   'kwargs': {'min_out' : choice([null,0]),
-					  'max_out' : choice([1,null])}}
-					      
+                        'n_filters' : choice([16,32,64,128]),
+                        'generate': ('random:uniform',
+                            {'rseed': choice([21, 22, 23, 24, 25])})},
+           'kwargs': {'min_out' : choice([null,0]),
+                      'max_out' : choice([1,null])}}
+
 filter3 = {'initialize': {'filter_shape' : choice([(3,3),(5,5),(7,7),(9,9)]),
-					   'n_filters' : choice([16,32,64,128,256]),
-					   'generate': ('random:uniform',{'rseed':42})},
-		   'kwargs': {'min_out' : choice([null,0]),
-					  'max_out' : choice([1,null])}}					      
+                           'n_filters' : choice([16,32,64,128,256]),
+                           'generate': ('random:uniform',
+                               {'rseed': choice([31, 32, 33, 34, 35])})},
+           'kwargs': {'min_out' : choice([null,0]),
+                      'max_out' : choice([1,null])}}
             
 layers = [[('lnorm', lnorm)],
           [('fbcorr', filter1),
@@ -149,6 +152,4 @@ layers_activ_hetero = [{'lnorm' : lnorm},
 					                       activ_multiple_uniform]),
 					    'lpool' : lpool,
 					    'lnorm' : lnorm}
-			 		  ] 
-            
-        
+			 		  ]
