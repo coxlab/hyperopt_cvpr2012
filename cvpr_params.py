cba471e64802c40d925220faa593bfcfd35b0bb5
Daniel Yamins <dyamins@gmail.com>
2011-11-11 09:15:22 -0500

    nothing

--- cvpr_params.py
+++ cvpr_params.py
@@ -1,6 +1,6 @@
 import copy
 
-def string(s): 
+def string(s):
     return repr(s).replace("'",'"')
 
 ##Plain vanilla params  -- from FG11 paper
@@ -20,45 +20,45 @@ class TRUE(object):
         return 'true'
 true = TRUE()
 
-        
+
 def repr(x):
     if isinstance(x,str):
         return '"' + str(x) + '"'
     else:
         return x.__repr__()
-        
+
 class gObj(object):
     def __init__(self,*args,**kwargs):
         self.args = args
         self.kwargs = kwargs
-        
+
     def __repr__(self):
         argstr = ', '.join([repr(x) for x in self.args])
-        kwargstr = ', '.join([str(k) + '=' + repr(v) for k,v in self.kwargs.items()]) 
-        
+        kwargstr = ', '.join([str(k) + '=' + repr(v) for k,v in self.kwargs.items()])
+
         astr = argstr + (', ' + kwargstr if kwargstr else '')
         return self.name + '(' + astr + ')'
-        
-        
+
+
 class choice(gObj):
     name = 'choice'
 
 class uniform(gObj):
     name = 'uniform'
-    
+
 class gaussian(gObj):
-    name = 'gaussian'    
-    
+    name = 'gaussian'
+
 class lognormal(gObj):
-    name = 'lognormal'    
+    name = 'lognormal'
 
 class qlognormal(gObj):
-    name = 'qlognormal'    
+    name = 'qlognormal'
 
 class ref(object):
     def __init__(self,*p):
         self.path = p
-    
+
     def __repr__(self):
         return '.'.join(self.path)
 
@@ -69,15 +69,15 @@ lnorm = {'kwargs':{'inker_shape' : choice([(3,3),(5,5),(7,7),(9,9)]),
          'stretch' : choice([.1,1,10]),
          'threshold' : choice([.1,1,10])
          }}
-          
+
 lpool = {'kwargs': {'stride' : 2,
           'ker_shape' : choice([(3,3),(5,5),(7,7),(9,9)]),
           'order' : choice([1,2,10])
          }}
-        
-           
+
+
 activ =  {'min_out' : choice([null,0]),
-		  'max_out' : choice([1,null])}
+          'max_out' : choice([1,null])}
 
 filter1 = dict(
         initialize=dict(
@@ -116,7 +116,7 @@ layers = [[('lnorm', lnorm)],
           [('fbcorr', filter3),
            ('lpool', lpool),
            ('lnorm', lnorm)]
-         ]  
+         ]
 
 
 comparison = ['mult', 'absdiff', 'sqrtabsdiff', 'sqdiff']
@@ -128,55 +128,55 @@ activ_uniform = {'min_out' : choice([null, {'generate' : ('random:uniform',
                                                           {'rseed':42,
                                                            'mean':uniform(-.2,.2),
                                                            'delta':uniform(0,.2)}),
-                                                           
+
                                             }]),
                  'max_out' : choice([null, {'generate' : ('random:uniform',
                                                           {'rseed':42,
                                                            'mean':uniform(.8,1.2),
                                                            'delta':uniform(0,.2)}),
-                                                           
-                                            }]),                                       
+
+                                            }]),
                 }
-                
+
 activ_gaussian = {'min_out' : choice([null, {'generate' : ('random:normal',
                                                           {'rseed':42,
                                                            'mean':uniform(-.2,.2),
                                                            'stdev':uniform(0,.2)}),
-                                                           
+
                                             }]),
                  'max_out' : choice([null, {'generate' : ('random:normal',
                                                           {'rseed':42,
                                                            'mean':uniform(.8,1.2),
                                                            'stdev':uniform(0,.2)}),
-                                                           
-                                            }]),                                       
+
+                                            }]),
                 }
-                 
-       
-                 
+
+
+
 def activ_multiple_gen(dist,minargs,minkwargs,maxargs,maxkwargs,num):
     activ = {}
     activ['min_out'] = choice([null] + [{'generate': ('fixedvalues',{'values':[dist(*minargs,**minkwargs) for ind in range(k)]})} for k in range(1,num+1)])
-    activ['max_out'] = choice([null] + [{'generate': ('fixedvalues',{'values':[dist(*maxargs,**maxkwargs) for ind in range(k)]})} for k in range(1,num+1)]) 
+    activ['max_out'] = choice([null] + [{'generate': ('fixedvalues',{'values':[dist(*maxargs,**maxkwargs) for ind in range(k)]})} for k in range(1,num+1)])
     return activ
 
 activ_multiple_uniform = activ_multiple_gen(uniform,(-.2,.2),{},(1-.2,1+.2),{},5)
 
 filter1_h = copy.deepcopy(filter1)
 filter1_h['kwargs'] = choice([activ_uniform,
-					        activ_gaussian,
-					        activ_multiple_uniform])
-					        
+                            activ_gaussian,
+                            activ_multiple_uniform])
+
 filter2_h = copy.deepcopy(filter2)
 filter2_h['kwargs'] = choice([activ_uniform,
-					        activ_gaussian,
-					        activ_multiple_uniform])
-					        
+                            activ_gaussian,
+                            activ_multiple_uniform])
+
 filter3_h = copy.deepcopy(filter3)
 filter3_h['kwargs'] = choice([activ_uniform,
-					        activ_gaussian,
-					        activ_multiple_uniform])					        
-       
+                            activ_gaussian,
+                            activ_multiple_uniform])
+
 layers_h = [[('lnorm', lnorm)],
             [('fbcorr_h', filter1_h),
              ('lpool', lpool),
@@ -187,8 +187,8 @@ layers_h = [[('lnorm', lnorm)],
             [('fbcorr_h', filter3_h),
              ('lpool', lpool),
              ('lnorm', lnorm)]
-           ]  
-           
+           ]
+
 
 config_h = {'desc' : layers_h, 'comparison' : comparison}
 
@@ -197,21 +197,21 @@ activ_uniform2 = {'min_out' : choice([null, {'generate' : ('random:uniform',
                                                            {'rseed':42,
                                                            'mean':uniform(-.5,.1),
                                                            'delta':uniform(0,.2)}),
-                                                           
+
                                             }]),
-                 'max_out' : choice([null, 1]),                                       
+                 'max_out' : choice([null, 1]),
                 }
-                
+
 filter1_h2 = copy.deepcopy(filter1)
 filter1_h2['kwargs'] = activ_uniform2
-					        
+
 filter2_h2 = copy.deepcopy(filter2)
 filter2_h2['kwargs'] = activ_uniform2
 
 filter3_h2 = copy.deepcopy(filter3)
 filter3_h2['kwargs'] = activ_uniform2
-					    				        
-       
+
+
 layers_h2 = [[('lnorm', lnorm)],
             [('fbcorr_h', filter1_h2),
              ('lpool', lpool),
@@ -222,8 +222,8 @@ layers_h2 = [[('lnorm', lnorm)],
             [('fbcorr_h', filter3_h2),
              ('lpool', lpool),
              ('lnorm', lnorm)]
-           ]  
-           
+           ]
+
 
 config_h2 = {'desc' : layers_h2, 'comparison' : comparison}
 
@@ -232,21 +232,21 @@ activ_uniform3 = {'min_out' : choice([null, {'generate' : ('random:uniform',
                                                            {'rseed':42,
                                                            'mean':0,
                                                            'delta':uniform(0,.3)}),
-                                                           
+
                                             }]),
-                 'max_out' : choice([null, 1]),                                       
+                 'max_out' : choice([null, 1]),
                 }
-                
+
 filter1_h3 = copy.deepcopy(filter1)
 filter1_h3['kwargs'] = activ_uniform3
-					        
+
 filter2_h3 = copy.deepcopy(filter2)
 filter2_h3['kwargs'] = activ_uniform3
 
 filter3_h3 = copy.deepcopy(filter3)
 filter3_h3['kwargs'] = activ_uniform3
-					    				        
-       
+
+
 layers_h3 = [[('lnorm', lnorm)],
             [('fbcorr_h', filter1_h3),
              ('lpool', lpool),
@@ -257,30 +257,29 @@ layers_h3 = [[('lnorm', lnorm)],
             [('fbcorr_h', filter3_h3),
              ('lpool', lpool),
              ('lnorm', lnorm)]
-           ]  
-           
+           ]
 
 config_h3 = {'desc' : layers_h3, 'comparison' : comparison}
 
 activ_uniform4 = {'min_out' : choice([null, {'generate' : ('random:uniform',
                                                            {'rseed':42,
-                                                           'mean':0-0.2,
+                                                           'mean':-0.2,
                                                            'delta':uniform(0,.3)}),
-                                                           
+
                                             }]),
-                 'max_out' : choice([null, 1]),                                       
+                 'max_out' : choice([null, 1]),
                 }
-                
+
 filter1_h4 = copy.deepcopy(filter1)
 filter1_h4['kwargs'] = activ_uniform4
-				        
+
 filter2_h4 = copy.deepcopy(filter2)
 filter2_h4['kwargs'] = activ_uniform4
 
 filter3_h4 = copy.deepcopy(filter3)
 filter3_h4['kwargs'] = activ_uniform4
-					    				        
-       
+
+
 layers_h4 = [[('lnorm', lnorm)],
             [('fbcorr_h', filter1_h4),
              ('lpool', lpool),
@@ -291,25 +290,25 @@ layers_h4 = [[('lnorm', lnorm)],
             [('fbcorr_h', filter3_h4),
              ('lpool', lpool),
              ('lnorm', lnorm)]
-           ]  
-           
+           ]
+
 
 config_h4 = {'desc' : layers_h4, 'comparison' : comparison}
 
 activ_uniform5 = {'min_out' : choice([null, uniform(-.4,.2)]),
-                 'max_out' : choice([null, 1]),                                       
+                 'max_out' : choice([null, 1]),
                 }
-                
+
 filter1_h5 = copy.deepcopy(filter1)
 filter1_h5['kwargs'] = activ_uniform5
-				        
+
 filter2_h5 = copy.deepcopy(filter2)
 filter2_h5['kwargs'] = activ_uniform5
 
 filter3_h5 = copy.deepcopy(filter3)
 filter3_h5['kwargs'] = activ_uniform5
-					    				        
-       
+
+
 layers_h5 = [[('lnorm', lnorm)],
             [('fbcorr_h', filter1_h5),
              ('lpool', lpool),
@@ -320,7 +319,7 @@ layers_h5 = [[('lnorm', lnorm)],
             [('fbcorr_h', filter3_h5),
              ('lpool', lpool),
              ('lnorm', lnorm)]
-           ]  
-           
+           ]
+
 
 config_h5 = {'desc' : layers_h5, 'comparison' : comparison}
