41f52a86419ac3c546424cd0fd3f36e97ef2686c
Dan Yamins <dyamins@gmail.com>
2011-12-14 23:29:02 -0500

    plots

--- lfw_model_exploration.py
+++ lfw_model_exploration.py
@@ -3,7 +3,10 @@
 """
 import copy
 import itertools
+
+import numpy as np
 import hyperopt.genson_bandits as gb
+import pymongo as pm
 
 import cvpr_params
 from lfw import get_performance, DEFAULT_COMPARISONS
@@ -84,8 +87,8 @@ class LFWBandit(gb.GensonBandit):
 
 
 ######
-
-orders = choice([[list(o[:ind]),list(o[ind:])] for o in list(itertools.permutations(['lpool','activ','lnorm'])) for ind in range(4)])
+order_choices = [[list(o[:ind]),list(o[ind:])] for o in list(itertools.permutations(['lpool','activ','lnorm'])) for ind in range(4)]
+orders = choice(order_choices)
 values = [{'lnorm':lnorm, 'lpool':lpool, 'activ':activ, 'filter':filter1},
           {'lnorm':lnorm, 'lpool':lpool, 'activ':activ, 'filter':filter2},
           {'lnorm':lnorm, 'lpool':lpool, 'activ':activ, 'filter':filter3}]
@@ -112,3 +115,34 @@ class LFWBanditModelExploration(gb.GensonBandit):
         config = get_model_config(config)
         result = get_performance(None, config)
         return result
+
+def make_plot():
+    conn = pm.Connection()
+    db = conn['hyperopt']
+    Jobs = db['jobs']
+    exp_key0 = 'lfw_model_exploration.LFWBandit/hyperopt.theano_bandit_algos.TheanoRandom'
+    H0 = 1-np.array([x['result']['loss'] for x in Jobs.find({'exp_key': exp_key0, 'state':2})])
+    NF0 =  np.array([x['result']['data']['mult'][0]['n_features'] for x in Jobs.find({'exp_key': exp_key0, 'state':2})])
+    
+    exp_key = 'lfw_model_exploration.LFWBanditModelExploration/hyperopt.theano_bandit_algos.TheanoRandom'
+    Qs = [{'exp_key':exp_key,'state':2,'spec.order':o} for o in order_choices]
+    
+    L = [1-np.array([x['result']['loss'] for x in Jobs.find(q,fields=['result.loss'])]) for q in Qs]
+    NF = [np.array([x['result']['data']['mult'][0]['n_features'] for x in Jobs.find(q,fields=['result.data.mult'])]) for q in Qs]
+        
+    import matplotlib.pyplot as plt
+    plt.figure()
+    plt.boxplot([l-H0.mean() for l in L])
+    plt.plot(range(1,len(L)+1),[np.mean(l) - H0.mean() for l in L],color='green')
+    plt.scatter(range(1,len(L)+1),[np.mean(l) - H0.mean() for l in L])
+    od = {'lpool': 'p', 'activ': 'a', 'lnorm': 'n'}
+    order_labels = [','.join([od[b] for b in Before]) + '|' + ','.join([od[b] for b in After]) for (Before, After) in order_choices]
+    plt.xticks(range(1,len(L)+1),order_labels, rotation=60)
+    plt.title('Model form exploration')
+    plt.ylabel('Performance relative to usual L3 on LFW')
+    plt.xlabel('Architecture tag')
+    plt.savefig('model_exploration_boxplots.png')
+    
+
+ 
+    return L, (H0, NF0)
